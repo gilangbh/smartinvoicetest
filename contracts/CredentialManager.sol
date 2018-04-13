c0ea0278d59ee0d8a0fff7579825e566defa2d05
Fengkie Junis <fengkiejunis@yahoo.co.id>
2018-04-13 14:01:53 +0700

    Update CredentialManager.sol
    In current code of this contract, there is a vulnerability that anybody can set any address to any role just by calling setRole

--- contracts/CredentialManager.sol
+++ contracts/CredentialManager.sol
@@ -8,8 +8,14 @@ contract CredentialManager {
     //3 - investor
 
     mapping(uint => mapping(address => bool)) private userRoles;
+    
+    function CredentialManager() {
+        //set initial admin
+        userRoles[0][msg.sender] = true;
+    }
 
     function setRole(uint role, address user) {
+        require(isInRole(0, msg.sender)); //check whether the sender is admin
         userRoles[role][user] = true;
     }
 
