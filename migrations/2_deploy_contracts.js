20672e4d78261f105eb45616c517d9203f2dd4d4
Gilang Bhagaskara <sciters@gmail.com>
2018-05-21 12:14:42 +0700

    fixed deployer

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,20 +4,13 @@ var Verification = artifacts.require("./Verification.sol");
 var InvoiceToken = artifacts.require("./InvoiceToken.sol");
 var CredentialManager = artifacts.require("./CredentialManager.sol");
 var InvestorCommisionContract = artifacts.require("./InvestorCommisionContract.sol");
-<<<<<<< HEAD
-=======
 var SupplierCommisionContract = artifacts.require("./SupplierCommisionContract.sol");
->>>>>>> 0bec0d2854cc98a0bdd67bda9ea776499618a0bc
 
 module.exports = function(deployer) {
   deployer.deploy(Chainy);
   deployer.deploy(Verification);
   deployer.deploy(CredentialManager);
   deployer.deploy(InvoiceToken);
-<<<<<<< HEAD
-  deployer.deploy(InvestorCommisionContract, CredentialManager);
-=======
   deployer.deploy(InvestorCommisionContract);
   deployer.deploy(SupplierCommisionContract)
->>>>>>> 0bec0d2854cc98a0bdd67bda9ea776499618a0bc
 };
