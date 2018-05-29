d46c6da87d79c6f6cc8d9f4f48c272e42919c67a
Gilang Bhagaskara <sciters@gmail.com>
2018-05-29 22:09:25 +0700

    updated verification

--- contracts/Verification.sol
+++ contracts/Verification.sol
@@ -12,7 +12,7 @@ contract Verification {
     Chainy private chainy;
     bool private isChainySet;
     
-    event VerificationStatusEvent(string chainy, bool verificationStatus, bool isAccepted);
+    event VerifyEvent(string chainy, bool verified, address verifier);
 
     function Verification() public {
         
@@ -27,13 +27,13 @@ contract Verification {
     function verifyFromBuyer(string _chainy, address _buyer) public {
         invoiceBuyer[_chainy] = _buyer;
         
-        VerificationStatusEvent(_chainy, getVerificationStatus(_chainy),true); 
+        VerifyEvent(_chainy,true,_buyer); 
     }
 
     function verifyFromAdmin(string _chainy, address _admin) public {
         invoiceAdminVerifier[_chainy] = _admin;
         
-        VerificationStatusEvent(_chainy, getVerificationStatus(_chainy),true);
+        VerifyEvent(_chainy, true, _admin);
     }
 
     
@@ -41,18 +41,18 @@ contract Verification {
         invoiceBuyer[_chainy] = 0x0;
         rejectedByBuyer[_chainy] = _buyer;
         
-        VerificationStatusEvent(_chainy, getVerificationStatus(_chainy),false); 
+        VerifyEvent(_chainy, false, _buyer); 
     }
 
     function rejectFromAdmin(string _chainy, address _admin) public {
         invoiceAdminVerifier[_chainy] = 0x0;
         rejectedByAdmin[_chainy] = _admin;
         
-        VerificationStatusEvent(_chainy, getVerificationStatus(_chainy),false);
+        VerifyEvent(_chainy, false, _admin);
     }
 
     function getVerificationStatus(string _chainy) public constant returns(bool) {
-        if(invoiceBuyer[_chainy] != 0x0 && invoiceAdminVerifier[_chainy] != 0x0) {
+        if (invoiceBuyer[_chainy] != 0x0 && invoiceAdminVerifier[_chainy] != 0x0){
             return true;
         }
         return false;
